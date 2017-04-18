3c25a6bf25db953b8b8300e810946ee11dc41070
smckinney0706 <smckinney0706@gmail.com>
2017-04-17 19:41:02 -0500

    final commit. Got the API working with Angular. Replaced & entity in JSON file. Added a Second Page option. Fixed structure with title, thumbnail, ingredients and linked website for recipes.

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,56 @@
 
 var app = angular.module('foodApp', []);
 app.controller('foodCtrl', function($scope, $http){
+
+	$("#pageContain").hide();
+
 	$scope.showFood = function(){
-		$http.get("http://www.recipepuppy.com/api/?q=" + $("#searchRec").val())
-		.then(function(response){
+		$("#pageContain").show();
+		$http.get("http://www.recipepuppy.com/api/?i=" + $("#searchIng").val() + "&q=" + $("#searchRec").val() + "&onlyImages=1")
+		.then(function(response, status){
 			$scope.food = response.data.results;
-			// console.log($scope.food);
-			$scope.thumbnail = response.data.results[0].thumbnail;
+			angular.forEach($scope.food, function(value,key){
+				value.title = value.title.replace('&amp;', '&');
+			});
+			// alert(response.status);
+			// console.log( response.data.results[0].ingredients );
+			//$scope.thumbnail = response.data.results[0].thumbnail;
 		});
 	};
+
+	$scope.changePage1 = function(){
+		$http.get("http://www.recipepuppy.com/api/?i=" + $("#searchIng").val() + "&q=" + $("#searchRec").val() + "&onlyImages=1" + "&p=1")
+		.then(function(response, status){
+			$scope.food = response.data.results;
+			angular.forEach($scope.food, function(value,key){
+				value.title = value.title.replace('&amp;', '&');
+			});
+			// alert(response.status);
+			// console.log( response.data.results[0].ingredients );
+			//$scope.thumbnail = response.data.results[0].thumbnail;
+		});
+	};
+
+
+	$scope.changePage2 = function(){
+		$http.get("http://www.recipepuppy.com/api/?i=" + $("#searchIng").val() + "&q=" + $("#searchRec").val() + "&onlyImages=1" + "&p=2")
+		.then(function(response, status){
+			$scope.food = response.data.results;
+			angular.forEach($scope.food, function(value,key){
+				value.title = value.title.replace('&amp;', '&');
+			});
+			// alert(response.status);
+			// console.log( response.data.results[0].ingredients );
+			//$scope.thumbnail = response.data.results[0].thumbnail;
+		});
+
+	};
+
+	
+	// $scope.removeAnds = function(){
+	// 	$("h2").each(function() {
+	// 		var text = $(this).text();
+	// 		$(this).text(text.replace('&amp;','&'));
+	// 	});
+	// };
 });
